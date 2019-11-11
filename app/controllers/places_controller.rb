bca0e3b841ce1c428632ef97cc09d08418ca83f5
TJBender <tiana.bender01@gmail.com>
2019-11-11 16:16:02 -0500

    added more mvc stuff

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,31 +1,12 @@
 class PlacesController < ApplicationController
 
-    before_action :findPlace, only: [:show, :edit, :update]
-
-    def new
-    end
-
-    def create
-    end
-
-    def index
-    end
+    before_action :findPlace, only: [:show]
 
     def show
     end
 
-    def edit 
-    end
 
-    def update
-    end
-
-
-    private 
-
-    def placeParams
-        params.require().permit()
-    end
+    private
 
     def findPlace
         @place = Place.find(params[:id])
