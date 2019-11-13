5094fd421c392d1bebfc493ed13de8972f00d4a7
GeneHFang <ghl234@nyu.edu>
2019-11-13 11:49:08 -0500

    forgot to save stuff

--- app/controllers/expeditions_controller.rb
+++ app/controllers/expeditions_controller.rb
@@ -26,6 +26,28 @@ class ExpeditionsController < ApplicationController
 
     def show
         @place = @expedition.place
+        placename = @place.name
+
+        #check progress and display appropriate image
+        # @img = "assets/images/#{placename}/img#{@place.progress}.jpg""
+
+        if params[:explore]
+            #check if early or later area (how do we check for this
+                        # new column called progress in Place? increment each time explorer :move_on ) 
+            #assign modifier based on ^
+            #roll for chance of a discovery (w modifier)
+            #if discovery, then roll for what kind of outcome (w modifier)
+            #decrement explorer rations
+        elsif params[:move_on]
+            #increment progress 
+            #check if guide
+            #decrement (^more if) rations 
+
+        end
+
+       
+
+            
     end
 
     def edit 
